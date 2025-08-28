dee8cbb170063500b3f2d11f3bf4e0112ffd78b3
stephengeller <stephen+git@geller.dev>
2025-08-29 00:04:47 +1000

    chore: deprecate husky shim and update yarn.lock
    - Deprecate .husky/_/husky.sh by printing a deprecation notice and guiding removal of the shim lines
    - Update yarn.lock with multiple dependency updates (e.g., ansi-escapes 7.0.0, chalk 5.6.0, environment 1.1.0, lint-staged 15.5.2, log-update 6.1.0, etc.)

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
-# husky shim (created by setup)
+echo "husky - DEPRECATED
 
+Please remove the following two lines from $0:
+
+#!/usr/bin/env sh
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
+
+They WILL FAIL in v10.0.0
+"
